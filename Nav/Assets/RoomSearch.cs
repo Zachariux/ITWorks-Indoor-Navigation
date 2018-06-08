a3f97ffe8c117d75dc171c5afbbbccebb106a249
Leon Wilson <leon.wilson@student.tafesa.edu.au>
2018-06-08 16:35:15 +0930

    Created branch feature_searchRooms
    Added basic search functionality

---
+++ Nav/Assets/RoomSearch.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class RoomSearch : MonoBehaviour {
+
+    public List<Room> Rooms = new List<Room>();
+
+    public InputField roomASearch;
+    public InputField roomBSearch;
+
+    public GameObject dropDownControlHolder;
+    public DropDown dropdwnScript;
+
+    public DropDown roomADrop;
+    public DropDown roomBDrop;
+
+    public List<string> k = new List<string>();
+
+    // Use this for initialization
+    void Start () {
+
+        dropdwnScript = dropDownControlHolder.GetComponent<DropDown>();
+
+        roomASearch.onValueChanged.AddListener(delegate
+        {
+            roomASearchValueChanged(roomASearch);
+        });
+
+        //roomASearch.on.AddListener(delegate
+        //{
+        //    roomASearchValueInput(roomAInput);
+        //});
+
+        roomBSearch.onValueChanged.AddListener(delegate
+        {
+            roomBSearchValueChanged(roomBSearch);
+        });
+
+    }
+	
+    void roomASearchValueChanged(InputField aChange)
+    {
+        //Debug.Log("Testing String");
+        try
+        {
+            string roomAInput = roomASearch.text;
+            if (Rooms.FirstOrDefault(item => item.roomName == roomAInput) != null)
+            {
+                       Room searchTarget = Rooms.FirstOrDefault(item => item.roomName == roomAInput);
+                            dropdwnScript.ClassA_dropd.ClearOptions();
+                            k.Add(searchTarget.name);
+                            dropdwnScript.ClassA_dropd.AddOptions(k);
+            }
+                    
+            
+        }
+        catch
+        {
+
+        }
+    }
+
+    void roomASearchValueInput(InputField aInput)
+    {
+
+    }
+
+    void roomBSearchValueChanged(InputField bChange)
+    {
+        //Debug.Log("Testing String");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
