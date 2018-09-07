6b4bc09c267312dbf5ff94a9b7e1aa9c4815c059
Leon Wilson <leon.wilson@student.tafesa.edu.au>
2018-09-07 15:19:01 +0930

    Added missing QRscanner Assets
    Added Button to change scene

---
+++ Nav/Assets/Scripts/navtoqr.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class navtoqr : MonoBehaviour {
+
+	public void changeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
